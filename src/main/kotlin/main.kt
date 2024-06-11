85a3f9b4b675deac0bfd0b6a3bc3d25265394977
Pavel <pmshuv975@gmail.com>
2024-06-11 23:54:50 +0300

    initial commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,38 @@
 package ru.netology
 
 fun main() {
-    val amount = 100_000 // сумма перевода в рублях
-    val minCommission = 35 // минимальная комиссия
 
-    var transferAmount: Int = (amount * 0.0075).toInt()
-
-    if (transferAmount < minCommission) {
-        transferAmount = minCommission
+    val typeCard = "Мир"
+    val lastTransfers = 0
+    val transfer = 150_000
+    val dailyLimit = 150_000
+    val monthlyLimit = 600_000
+    val commission = calculateCommission(typeCard, lastTransfers, transfer)
+    if (transfer <= dailyLimit && transfer <= monthlyLimit) {
+        println("Сумма перевода: $transfer" + "\nКомиссия: $commission")
     }
+    else {
+        println("Превышен лимит. Перевод отменен")
+        return
+    }
+}
 
-    println(
-        """Сумма перевода: $amount руб.
-            Комиссия: $transferAmount руб."""
-    )
+fun calculateCommission(typeCard: String, lastTransfers: Int, transfer: Int):
+        Int {
+    when (typeCard) {
+        "Мир" -> return 0
+        "MasterCard" -> {
+            if ((lastTransfers + transfer) > 75_000) {
+                return ((transfer - 75_000) * 0.006 + 20).toInt()
+            } else {
+                return 0
+            }
+        }
+
+        "Visa" -> (transfer * 0.0075).toInt()
+        else -> {
+            return 0
+        }
+    }
+    return 0
 }
